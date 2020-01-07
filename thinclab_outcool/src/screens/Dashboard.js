a0794f906f6c2287d77169f07ac42285d9742522
FlorentCmb <florent.cmb77@gmail.com>
2020-01-07 15:19:34 +0100

    Add Meteo file, and meteo infos

--- thinclab_outcool/src/screens/Dashboard.js
+++ thinclab_outcool/src/screens/Dashboard.js
@@ -3,11 +3,14 @@ import React from 'react'
 
 // Style
 import './Dashboard.css'
+import Meteo from '../components/Meteo'
 
 const Dashboard = () => {
     return (
         <div className="Dashboard">
-            <div className="Dashboard-Meteo">Meteo</div>
+            <div className="Dashboard-Meteo">
+                <Meteo />
+            </div>
             <div className="Dashboard-Schedule">Schedule</div>
             <div className="Dashboard-List">List</div>
         </div>
