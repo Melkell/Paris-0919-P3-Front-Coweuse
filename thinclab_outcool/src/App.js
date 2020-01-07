d523b48b4711a0f880f36e215bc19b31efc95daf
FlorentCmb <florent.cmb77@gmail.com>
2020-01-07 09:21:18 +0100

    Clean React project

--- thinclab_outcool/src/App.js
+++ thinclab_outcool/src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import './App.css';
+// Librairies
+import React from 'react'
+
+// Screens & components
+
+// Styles
+import './App.css'
 
 const App = () => {
   return (
     <div className="App">
-      <div className="App-header">
-        <h1>Projet Outcool - Thinclab</h1>
-      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
