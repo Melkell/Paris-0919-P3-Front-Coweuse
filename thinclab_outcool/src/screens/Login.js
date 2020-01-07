9b98dd8fb8262dfac5436c2a982da79286aa81b9
FlorentCmb <florent.cmb77@gmail.com>
2020-01-07 10:49:17 +0100

    Add controlled inputs

--- thinclab_outcool/src/screens/Login.js
+++ thinclab_outcool/src/screens/Login.js
@@ -1,17 +1,33 @@
 // Librairies
-import React from 'react'
+import React, { useState } from 'react'
 
 // Styles
 import './Login.css'
 
 const Login = () => {
+
+    // States
+    const [emailInput, setemailInput] = useState("")
+    const [passwordInput, setpasswordInput] = useState("")
+
+    // Input handlers
+    const emailHandler = e => {
+        const content = e.target.value
+        setemailInput(content)
+    }
+
+    const passwordHandler = e => {
+        const content = e.target.value
+        setpasswordInput(content)
+    }
+
     return (
         <div className="Login">
-            <h4>Outcool - Connextion</h4>
+            <h4 className="Login-Title">Outcool - Connexion</h4>
             <form className="Login-Form">
-                <input className="Login-Input" type="email" name="mail" placeholder="Email" />
-                <input className="Login-Input" type="password" name="password" placeholder="Password" />
-                <input className="Login-Submit" type="submit" value="Submit" />
+                <input className="Login-Input" type="email" placeholder="Email" value={emailInput} onChange={emailHandler} />
+                <input className="Login-Input" type="password" placeholder="Password" value={passwordInput} onChange={passwordHandler} />
+                <input className="Login-Submit" type="submit" value="Se connecter" />
             </form>
         </div>
     )
