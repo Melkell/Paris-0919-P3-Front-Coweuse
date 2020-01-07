80e90e30ab330259343495d49eabe2529f5d288e
FlorentCmb <florent.cmb77@gmail.com>
2020-01-07 09:28:30 +0100

    Link CSS to Login file and add React import

---
+++ thinclab_outcool/src/screens/Login.js
@@ -0,0 +1,15 @@
+// Librairies
+import React from 'react'
+
+// Styles
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div>
+            Login page
+        </div>
+    )
+}
+
+export default Login
