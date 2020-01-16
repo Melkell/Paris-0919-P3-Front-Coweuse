553f969ceafc51266c9c183c6d974caea82281fa
Melkell <mel.kelly.maisonnave@gmail.com>
2020-01-16 22:30:10 +0100

    test météo

--- thinclab_outcool/src/components/Meteo.js
+++ thinclab_outcool/src/components/Meteo.js
@@ -1,114 +1,30 @@
 // Librairies
 import React, { useState, useEffect } from 'react'
-import Axios from 'axios'
 
 // Styles
 import './Meteo.css'
 
 const Meteo = () => {
 
-    const [currentDate, setCurrentDate] = useState('')
-    const [meteo, setMeteo] = useState(null)
-    const [isReady, setIsReady] = useState(false)
-    let currentMeteo
+    let request = require('request');
 
-    const getDate = () => {
-        let date = new Date()
-        // Getting the day
-        let day = date.getDate()
-        if (day < 10) {
-            day = "0" + day
-        }
-        // Getting the month
-        let month = date.getMonth()
-        if (month < 10) {
-            month = "0" + (month + 1)
-        }
-        // Getting the year
-        let year = date.getFullYear()
-        // Getting the hour
-        let hour = date.getHours()
-        if (hour < 10) {
-            hour = "0" + hour
-        }
-        // The meteo API return datas when hours % 3 = 1
-        switch (hour % 3) {
-            case 0:
-                hour += 1
-                break
-            case 2:
-                hour += 2
-                break
-            default:
-                break
-        }
-
-        // Set the date to the right format and give its value to the state
-        setCurrentDate(`${year}-${month}-${day} ${hour}:00:00`)
-
-        // Get the meteo
-        Axios.get("https://www.infoclimat.fr/public-api/gfs/json?_ll=48.85341,2.3488&_auth=VU9QRwR6XX9TfgYxBXMKIwRsVWABdwIlC3cHZF86VClWPVU0VjZWMFM9VitSfQo8BSgEZ1phCTkKYVYuXC4FZFU%2FUDwEb106UzwGYwUqCiEEKlU0ASECJQtgB2FfLFQ1VjBVNFYrVjVTPFYqUmoKPwUpBHtaZAk0Cm5WOVwyBW9VMlA0BGVdOVMjBnsFMAprBDZVMQE4AjsLOQcxX2dUYVY3VTlWNlY2UyJWMVJiCjcFMwRtWmwJNgpsVi5cLgUfVUVQKQQnXX1TaQYiBSgKawRpVWE%3D&_c=d30a3dd0a6459d0695d16604536bb29c")
-            .then((result) => {
-                setMeteo(result.data)
-                setIsReady(true)
-            })
-    }
-
-    const getCurrentMeteo = (array) => {
-        for (let [key, value] of Object.entries(array)) {
-            if (key === currentDate) {
-                console.log(value)
-                currentMeteo = value
-            }
-        }
-    }
-
-    useEffect(() => {
-        getDate()
-    }, [])
-
-    // Change background color according to the current nebulosity
-    const changeColor = () => {
-        if (currentMeteo) {
-            let x = 50 + (currentMeteo.nebulosite.totale / 100 * 30)
-            let y = 215 + (currentMeteo.nebulosite.totale / 100 * (-110))
-            let z = 255 + (currentMeteo.nebulosite.totale / 100 * (-145))
-            document.querySelector('.Meteo-Card').style.background = `rgb(${x}, ${y}, ${z})`
-        }
-    }
-    // Change meteo description
-    const meteoDescription = () => {
-        if (currentMeteo) {
-            if (currentMeteo.nebulosite.totale < 33) {
-                return 'partiellement couvert'
-            }
-            else if (currentMeteo.nebulosite.totale > 33 && currentMeteo.nebulosite.totale < 50) {
-                return 'plutôt nuageux'
-            }
-            else if (currentMeteo.nebulosite.totale > 50) {
-                return 'très nuageux'
-            }
-        }
-        navigator.getCurrentPosition((position) => {
-            let lat = position.coords.latitude
-            let lng = position.coords.longitude
-            Axios.get(`https://www.prevision-meteo.ch/services/json/lat=${lat}lng=${lng}`)
-                .then((result)=> console.log(result))
-        })
-    }
+    let api_key = 'c0166b37dd54444edbfe4b624fbe5640';
+    let city = 'paris';
+    let url = `http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?appid=${api_key}&
+    fill_bound=true&opacity=0.6&palette=-65:821692;-55:821692;-45:821692;-40:821692;
+    -30:8257db;-20:208cec;-10:20c4e8;0:23dddd;10:c2ff28;20:fff028;25:ffc228;30:fc8014`
+    
+    request(url, function (err, response, body) {
+      if(err){
+        console.log('error:', err);
+      } else {
+        let weather = JSON.parse(body)
+        console.log(weather);
+      }
+    });
 
     return (
         <div className="Meteo">
-            {console.log(currentDate)}
-            {isReady ? getCurrentMeteo(meteo) : ''}
-            <div className="Meteo-Card" onLoad={changeColor()}>
-                {currentMeteo !== undefined ? (
-                    <div className="Meteo-Info">
-                    <p className="Meteo-Temperature">{Math.round((currentMeteo.temperature.sol - 273.5) * 100) / 100} °C</p>
-                        <p>Ciel {meteoDescription()}</p>
-                    </div>
-                ) : ''}
-            </div>
         </div>
     )
 }
