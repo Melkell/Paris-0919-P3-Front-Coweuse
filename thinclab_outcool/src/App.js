ca0242388dccca9a4313240dd37b09ef6c1fc72e
Melkell <mel.kelly.maisonnave@gmail.com>
2020-01-17 05:50:32 +0100

    hard to work

--- thinclab_outcool/src/App.js
+++ thinclab_outcool/src/App.js
@@ -2,13 +2,16 @@
 import React from 'react'
 
 // Screens & components
-
+import TypeItin from './components/TypeItin'
+import Header from './components/Header'
 // Styles
 import './App.css'
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
+      <TypeItin />
     </div>
   )
 }
---
+++ thinclab_outcool/src/components/TypeItin.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+import './TypeItin.css'
+
+
+const typeItin = () => {
+
+
+
+    return(
+        <div className="select-Prod">
+            <select>
+                <option> Blé </option>
+                <option> Brocoli </option>
+                <option> Tomate </option>
+                <option> Céréal </option>
+            </select>
+        </div>
+    )
+}
+
+export default typeItin
