224d052960487115993cd03bfd696b172cf1652a
Melkell <mel.kelly.maisonnave@gmail.com>
2020-01-17 17:38:53 +0100

    meteo

--- thinclab_outcool/src/screens/Dashboard.js
+++ thinclab_outcool/src/screens/Dashboard.js
@@ -19,7 +19,9 @@ const Dashboard = () => {
 
 	return (
 		<div className="Dashboard">
-			<div className="Dashboard-Meteo">Meteo</div>
+			<div className="Dashboard-Meteo">
+				<Meteo />
+			</div>
 			<div className="Dashboard-Schedule">
 				<Agenda addMission={addMission}/>
 			</div>
