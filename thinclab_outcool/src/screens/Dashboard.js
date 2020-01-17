91c4e9423ef72cfb74180b65e2e6fe40d4979a68
tschanomis <tschan.omis@gmail.com>
2020-01-17 17:14:18 +0100

    fix conflict

--- thinclab_outcool/src/screens/Dashboard.js
+++ thinclab_outcool/src/screens/Dashboard.js
@@ -9,7 +9,6 @@ import Meteo from '../components/Meteo'
 import Listemission from './Listemission';
 
 const Dashboard = () => {
-<<<<<<< HEAD
 	const [addMission, setaddMission] = useState(false);
 	const [items, setItems] = useState([]);
 
@@ -29,21 +28,6 @@ const Dashboard = () => {
 			</div>
 		</div>
 	)
-=======
-    return (
-        <div className="Dashboard">
-            <div className="Dashboard-Meteo">
-                <Meteo />
-            </div>
-            <div className="Dashboard-Schedule"> <div className="Dashboard-Agenda">
-                <Agenda />
-            </div></div>
-            <div className="Dashboard-List"> <div className="Dashboard-Listemission">
-                <Listemission />
-            </div></div>
-        </div>
-    )
->>>>>>> 0e079229f3d531876697d82190bb7ce4a6954e53
 }
 
 export default Dashboard
