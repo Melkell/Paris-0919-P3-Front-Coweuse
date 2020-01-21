a559981deffdab1457db1086ba80738230870478
Melkell <mel.kelly.maisonnave@gmail.com>
2020-01-21 16:01:16 +0100

    try to add card mission

--- thinclab_outcool/src/components/TypeItin.js
+++ thinclab_outcool/src/components/TypeItin.js
@@ -8,14 +8,36 @@ const TypeItin = () => {
 
     // This state will receive all production types
     const [prodType, setProdType] = useState(null)
+    const [ressourceType, setRessourceType] = useState(null)
 
     // Select handler
     const handleSelect = () => {
         document.querySelector('.TypeItin-Select').addEventListener('change', e => {
             // Here we can get the selected option
-            console.log(e.target.value)
+            const prodType = e.target.value
+            switch (prodType) {
+                case "blé":
+                case "avoine":
+                case "maïs":
+                    setRessourceType("Céréale")
+                    break
+                case "betterave":
+                case "tomate":
+                case "carotte":
+                case "patate":
+                    setRessourceType("Végétal")
+                    break
+                case "bovidés":
+                case "volailles":
+                    setRessourceType("Animal")
+                    break
+                default:
+                    setRessourceType(null)
+            }
+        
         })
     }
+
 
     // Search the production types
     useEffect(() => {
@@ -25,12 +47,15 @@ const TypeItin = () => {
         handleSelect()
     }, [])
 
+
     return (
         <div className="select-prod">
             <h1 className="Itin"> Itinéraire </h1>
             <select className="TypeItin-Select">
+                <option value={null}>Choose a ressource</option>
                 {prodType !== null ? prodType.map(item => (<option key={item.toLowerCase()} value={item.toLowerCase()}>{item}</option>)) : 'Loading'}
             </select>
+            {ressourceType !== null ? changeRessourceType() : 'Waiting for a selection'}
         </div>
     )
 }
