8047189a36571409e75f48e4034d7eab06607c12
FlorentCmb <florent.cmb77@gmail.com>
2020-01-21 10:43:01 +0100

    Add dynamic Select tags

--- thinclab_outcool/src/components/Header.js
+++ thinclab_outcool/src/components/Header.js
@@ -7,17 +7,17 @@ const Header = () => {
 
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Thinklab</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Thinklab</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-item nav-link active" href="#">Outcool <span class="sr-only">(current)</span></a>
-                        <a class="nav-item nav-link" href="#">Dashboard</a>
-                        <a class="nav-item nav-link" href="#">Agenda</a>
-                        <a class="nav-item nav-link " href="#">3truc</a>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link active" href="#">Outcool <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link" href="#">Dashboard</a>
+                        <a className="nav-item nav-link" href="#">Agenda</a>
+                        <a className="nav-item nav-link " href="#">3truc</a>
                     </div>
                 </div>
             </nav>
--- thinclab_outcool/src/components/TypeItin.js
+++ thinclab_outcool/src/components/TypeItin.js
@@ -1,24 +1,29 @@
-import React from 'react'
-
-
+// Librairies
+import React, { useState, useEffect } from 'react'
+// Styles
 import './TypeItin.css'
 
 
-const typeItin = () => {
+const TypeItin = () => {
 
+    // This state will receive all production types
+    const [prodType, setProdType] = useState(null)
 
+    // Search the production types
+    useEffect(() => {
+        /* Request the backend here */
+        // Exemple
+        setProdType(['Blé', 'Avoine', 'Bovidés', 'Volailles'])
+    }, [])
 
-    return(
+    return (
         <div className="select-prod">
             <h1 className="Itin"> Itinéraire </h1>
             <select className="prod-type">
-                <option> Blé </option>
-                <option> Brocoli </option>
-                <option> Tomate </option>
-                <option> Céréal </option>
+                { prodType !== null ? prodType.map(item => (<option  key={item.toLowerCase()} value={item.toLowerCase()}>{item}</option>)) : 'Loading' }
             </select>
         </div>
     )
 }
 
-export default typeItin
+export default TypeItin
