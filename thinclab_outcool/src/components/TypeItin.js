81454d1f075733c7aa766b5a1caeb4c0fc037060
FlorentCmb <florent.cmb77@gmail.com>
2020-01-21 14:32:57 +0100

    Add select handler (to get the result of the option)

--- thinclab_outcool/src/components/TypeItin.js
+++ thinclab_outcool/src/components/TypeItin.js
@@ -9,18 +9,27 @@ const TypeItin = () => {
     // This state will receive all production types
     const [prodType, setProdType] = useState(null)
 
+    // Select handler
+    const handleSelect = () => {
+        document.querySelector('.TypeItin-Select').addEventListener('change', e => {
+            // Here we can get the selected option
+            console.log(e.target.value)
+        })
+    }
+
     // Search the production types
     useEffect(() => {
         /* Request the backend here */
         // Exemple
         setProdType(['Blé', 'Avoine', 'Bovidés', 'Volailles'])
+        handleSelect()
     }, [])
 
     return (
         <div className="select-prod">
             <h1 className="Itin"> Itinéraire </h1>
-            <select className="prod-type">
-                { prodType !== null ? prodType.map(item => (<option  key={item.toLowerCase()} value={item.toLowerCase()}>{item}</option>)) : 'Loading' }
+            <select className="TypeItin-Select">
+                {prodType !== null ? prodType.map(item => (<option key={item.toLowerCase()} value={item.toLowerCase()}>{item}</option>)) : 'Loading'}
             </select>
         </div>
     )
