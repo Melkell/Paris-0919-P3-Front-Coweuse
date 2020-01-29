66b966914140fea6b22996437250110f2c9f205a
tschanomis <tschan.omis@gmail.com>
2020-01-29 17:11:20 +0100

    conflict

--- thinclab_outcool/src/App.js
+++ thinclab_outcool/src/App.js
@@ -8,27 +8,16 @@ import TypeItin from './components/TypeItin'
 import './App.css'
 import Header from './components/Header'
 import Dashboard from './screens/Dashboard';
-<<<<<<< HEAD
-import DashboardAdmin from './screens/DashboardAdmin';
-import Form from './components/Form';
-import Formconnection from './components/Formconnection';
-=======
 import Form from './components/Form';
 import Formconnection from './components/Formconnection';
 import DashboardAdmin from './screens/DashboardAdmin';
 import Footer from './components/Footer';
 
->>>>>>> 8b0eedf1a97a4b251e3cf75b43d3816c5fb311ac
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-<<<<<<< HEAD
-      <DashboardAdmin />
-      <Dashboard />
-      {/* <Formconnection /> */}
-=======
       <Router>
         <Switch>
           <Route exact path='/dashboardCollab' component={Dashboard}/>
@@ -39,7 +28,6 @@ const App = () => {
         </Switch>
       </Router>
       
->>>>>>> 8b0eedf1a97a4b251e3cf75b43d3816c5fb311ac
     </div>
   )
 }
