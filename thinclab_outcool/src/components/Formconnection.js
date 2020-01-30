73729dad57dca90d9843867314cb478da7bfbf2f
Valentine Voyen <valentine.voyen1@gmail.com>
2020-01-30 09:49:13 +0100

    cleaned code on formConnection.js

--- thinclab_outcool/src/components/Formconnection.js
+++ thinclab_outcool/src/components/Formconnection.js
@@ -4,31 +4,29 @@ import logo from '../img/oc.png'
 
 
 const Formconnection = () => {
-    return (
-        <div>
-        <form class="form-style-9">
-        <img src={logo} class="logoForm"/>
-            <ul>
-                <li>
-                    <input type="email" name="field3" class="field-style field-full align-none" placeholder="Adresse mail" />
-                </li>
-                <li>
-                    <input type="password" name="field3" class="field-style field-full align-none" placeholder="Mot de passe" />
-                </li>
-                <select className="select-status">
-                    <option value="">--Choisir un statut--</option>
-                    <option value="admin">Administrateur</option>
-                    <option value="collab">Collaborateur</option>
-                </select>
-                <li>
-                    <input type="submit" value="Connexion" />
-                </li>
-                
-            </ul>
-        </form>
-        </div>
-
-    )
+  return (
+    <div>
+    <form class="form-style-9">
+      <img src={logo} class="logoForm"/>
+        <ul>
+          <li>
+            <input type="email" name="field3" class="field-style field-full align-none" placeholder="Adresse mail" />
+          </li>
+          <li>
+            <input type="password" name="field3" class="field-style field-full align-none" placeholder="Mot de passe" />
+          </li>
+          <select className="select-status">
+            <option value="">--Choisir un statut--</option>
+            <option value="admin">Administrateur</option>
+            <option value="collab">Collaborateur</option>
+          </select>
+          <li>
+            <input type="submit" value="Connexion" />
+          </li>
+        </ul>
+    </form>
+    </div>
+  )
 }
 
 
