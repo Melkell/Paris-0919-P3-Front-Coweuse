f64dde2d7168a9421e32dfd4f94cda6534f147a8
tschanomis <tschan.omis@gmail.com>
2020-02-03 15:05:04 +0100

    add array

--- thinclab_outcool/src/components/MissList.js
+++ thinclab_outcool/src/components/MissList.js
@@ -5,11 +5,9 @@ import List from "./List.js"
 import './MissList.css'
 
 const MissList = ({ getMission, missions, admin }) => {
-	const filter = 1
 	return (
 		<div className={admin ? "Misslist-admin" : "Misslist"}>
-			{/*missions.map(item => (<List key={item.id} props={item} getMission={getMission} admin={admin} />))*/}
-			{missions.filter(item => item.tache_id % filter == 0).map(item => (<List key={item.id} props={item} getMission={getMission} admin={admin} />))}
+			{missions.map(item => (<List key={item.id} props={item} getMission={getMission} admin={admin} />))}
 		</div>
 	)
 }
