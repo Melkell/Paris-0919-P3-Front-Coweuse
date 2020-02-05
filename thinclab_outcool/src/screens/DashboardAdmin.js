5e23365b8e3b2f20f6d3792fef6689b85f28fd52
Valentine Voyen <valentine.voyen1@gmail.com>
2020-02-05 10:48:57 +0100

    feature: (modal) added ressource modal on dashboard

--- thinclab_outcool/src/screens/DashboardAdmin.js
+++ thinclab_outcool/src/screens/DashboardAdmin.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import Agenda from '../components/Agenda'
 import MissList from '../components/MissList'
 import TypeItin from '../components/TypeItin'
+import Ressources from '../components/Ressources'
 
 import './Dashboard.css'
 
@@ -12,7 +13,8 @@ const DashboardAdmin = () => {
 	const [items, setItems] = useState([]);
 	const [itemIndex, setItemIndex] = useState(0);
 	const [filter, setFilter] = useState(false);
-	const [modalItineraire, setModalItineraire] = useState(false)
+	const [modalItineraire, setModalItineraire] = useState(false);
+	const [modalRessources, setModalRessources] = useState(false);
 
 	const admin = true;
 
@@ -23,6 +25,10 @@ const DashboardAdmin = () => {
 
 	const showItineraire = () => {
 		setModalItineraire(!modalItineraire);
+	}
+
+	const showRessources = () => {
+		setModalRessources(!modalRessources);
 	}
 
 	/*const getMission = (e) => {
@@ -37,7 +43,7 @@ const DashboardAdmin = () => {
 				<div className="Action">
 					<div className="action-left">
 						<div className="one" onClick={showItineraire}>Ajout Mission</div>
-						<div className="two">Ressources</div>
+						<div className="two" onClick={showRessources}>Ressources</div>
 					</div>
 					<div className="action-right"></div>
 				</div>
@@ -56,6 +62,9 @@ const DashboardAdmin = () => {
 			</div>
 			{modalItineraire ? <div className="Dashboard-modal" clickOutside={showItineraire}>
 				<TypeItin />
+			</div> : ''}
+			{modalRessources ? <div className="ressources-modal" clickOutside={showRessources}>
+			 <Ressources />
 			</div> : ''}
 		</div >
 	)
