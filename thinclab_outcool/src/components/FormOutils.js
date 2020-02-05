8bd7dac64f3bc24a8bd02b655031f3cb33973385
Valentine Voyen <valentine.voyen1@gmail.com>
2020-02-05 13:35:43 +0100

    modal fix:(modal) added a title to the form modals

--- thinclab_outcool/src/components/FormOutils.js
+++ thinclab_outcool/src/components/FormOutils.js
@@ -1,35 +1,56 @@
-import React from 'react'
-import './FormOutils.css'
-
-
+import React from "react";
+import "./FormOutils.css";
 
 const FormOutils = () => {
-    
-    return (
-        <form class="form-style-9">
-            <ul>
-                <li>
-                    <input type="text" name="field1" class="field-style field-split align-left" placeholder="Production" />
-                    <input type="email" name="field2" class="field-style field-split align-right" placeholder="Parcelle" />
+  return (
+    <div>
+      <h1>Ajout des outils</h1>
+      <form class="form-style-9">
+        <ul>
+          <li>
+            <input
+              type="text"
+              name="field1"
+              class="field-style field-split align-left"
+              placeholder="Production"
+            />
+            <input
+              type="email"
+              name="field2"
+              class="field-style field-split align-right"
+              placeholder="Parcelle"
+            />
+          </li>
+          <li>
+            <input
+              type="text"
+              name="field3"
+              class="field-style field-split align-left"
+              placeholder="Objectifs Dates"
+            />
+            <input
+              type="url"
+              name="field4"
+              class="field-style field-split align-right"
+              placeholder="Outils"
+            />
+          </li>
+          <li>
+            <input
+              type="text"
+              name="field3"
+              class="field-style field-full align-none"
+              placeholder="Utilisation des outils"
+            />
+          </li>
+          <li></li>
+          <li>
+            <input type="submit" value="Envoyer" />
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
+};
 
-                </li>
-                <li>
-                    <input type="text" name="field3" class="field-style field-split align-left" placeholder="Objectifs Dates" />
-                    <input type="url" name="field4" class="field-style field-split align-right" placeholder="Outils" />
-                </li>
-                <li>
-                    <input type="text" name="field3" class="field-style field-full align-none" placeholder="Utilisation des outils" />
-                </li>
-                <li>
-                </li>
-                <li>
-                    <input type="submit" value="Envoyer" />
-                </li>
-            </ul>
-        </form>
-
-    )
-}
-
-
-export default FormOutils
+export default FormOutils;
